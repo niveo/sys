754f9b1da16aff092b651d3313d8d556f292e287
amsraimar <amsraimar@gmail.com>
2024-06-25 10:53:14 -0300

    first commit

---
+++ src/test/java/br/com/ams/sys/SysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ams.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
